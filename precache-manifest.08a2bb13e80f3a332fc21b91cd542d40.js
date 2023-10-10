1fb7aa6bf9c76d61afd8a67e471f3d44a622c05c
Parthahere001 <parthahere001.com>
2023-10-10 18:50:51 +0530

    Updates

---
+++ precache-manifest.08a2bb13e80f3a332fc21b91cd542d40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b8ae77343be792ba2b619295ab6dae8",
+    "url": "/hostingwpt/index.html"
+  },
+  {
+    "revision": "86a286c91cc699da6a89",
+    "url": "/hostingwpt/static/css/main.7280c1ee.chunk.css"
+  },
+  {
+    "revision": "1230e3e482db060d59ee",
+    "url": "/hostingwpt/static/js/2.67fa1db6.chunk.js"
+  },
+  {
+    "revision": "d42641fb492fb11536f5b8dabcfcdde9",
+    "url": "/hostingwpt/static/js/2.67fa1db6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86a286c91cc699da6a89",
+    "url": "/hostingwpt/static/js/main.982f8384.chunk.js"
+  },
+  {
+    "revision": "13c5d7774f8edea6c668",
+    "url": "/hostingwpt/static/js/runtime-main.eddda7f2.js"
+  }
+]);
